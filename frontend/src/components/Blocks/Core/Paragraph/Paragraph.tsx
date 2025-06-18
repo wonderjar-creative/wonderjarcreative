83ec4228bb19405b537d05bf3ddd98242a6af5aa
Matthew Ediger <edigermatthew@gmail.com>
2025-06-17 22:12:30 -0600

    fix: remove key from component

--- frontend/src/components/Blocks/Core/Paragraph/Paragraph.tsx
+++ frontend/src/components/Blocks/Core/Paragraph/Paragraph.tsx
@@ -1,25 +1,28 @@
 import { Maybe, CoreParagraphBlockAttributes } from '@/gql/graphql';
-import { getBlockClasses, getBlockStyle } from '@/utils/getBlockComponents';
+import { getBlockClasses, getBlockStyleAttr } from '@/utils/getBlockComponents';
 
 type ParagraphProps = {
   attributes: CoreParagraphBlockAttributes;
   dynamicContent?: Maybe<string> | undefined;
-  key: number;
   originalContent?: Maybe<string> | undefined;
   saveContent?: Maybe<string> | undefined;
 };
 
-export default function Paragraph({attributes, dynamicContent, key, originalContent, saveContent}: ParagraphProps) {
+export default function Paragraph({
+  attributes,
+  dynamicContent,
+  originalContent,
+  saveContent
+}: ParagraphProps) {
   const { anchor, style } = attributes;
   const blockClasses = getBlockClasses(attributes, 'wp-block-paragraph');
-  const blockStyle = getBlockStyle(style);
+  const blockStyleAttr = getBlockStyleAttr(style);
 
   return (
     <p
-      key={key}
       {...(anchor && { id: anchor })}
       className={blockClasses}
-      {...(style && { style: blockStyle })}
+      {...(style && { style: blockStyleAttr })}
       dangerouslySetInnerHTML={{
         __html: (dynamicContent || saveContent || originalContent)?.replace(/<p[^>]*>|<\/p>/g, '') || '',
       }}
