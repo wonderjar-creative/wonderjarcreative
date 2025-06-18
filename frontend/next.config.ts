f306b864b108d3539641da5b27bf03a892f65794
Matthew Ediger <edigermatthew@gmail.com>
2025-06-17 21:59:16 -0600

    feat: add images domains

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['wonderjarcreative.local', 'wonderjarcreative.com']
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
