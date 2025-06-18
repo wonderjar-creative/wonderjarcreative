e1dd7f35c69e815e39ba322cf86c34e0aaa0ed02
Matthew Ediger <edigermatthew@gmail.com>
2025-06-17 22:21:00 -0600

    feat: add stylesCollector

--- frontend/src/components/Templates/Page/PageTemplate.tsx
+++ frontend/src/components/Templates/Page/PageTemplate.tsx
@@ -12,18 +12,22 @@ export default async function PageTemplate({ node }: TemplateProps) {
   const { page } = await fetchGraphQL<{ page: Page }>(print(PageQuery), {
     id: node.databaseId,
   });
-  const blockComponents = page.blocksJSON ? await getBlockComponents(page.blocksJSON) : [];
+  const { blocksJSON, featuredImage, title } = page;
+
+  const stylesCollector: string[] = [];
+  const blockComponents = blocksJSON ? await getBlockComponents(blocksJSON, featuredImage, stylesCollector) : [];
 
   return (
     <main className="py-8 md:py-12">
+      {stylesCollector.length > 0 && <style>{stylesCollector.join('\n')}</style>}
       <header className="container mx-auto mb-8 px-4">
-        <h1 className="text-3xl md:text-4xl lg:text-6xl">{page.title}</h1>
+        <h1 className="text-3xl md:text-4xl lg:text-6xl">{title}</h1>
       </header>
       { blockComponents && blockComponents.length > 0 ? (
         <div className="entry-content">
           {blockComponents}
         </div>
-      ): (
+      ) : (
         <p className="text-center text-lg text-gray-500">No content available.</p>
       )}
     </main>
