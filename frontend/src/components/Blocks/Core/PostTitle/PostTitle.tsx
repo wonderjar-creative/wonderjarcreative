c79dbd4f1767c5738660e81cf91627a94979e103
Matthew Ediger <edigermatthew@gmail.com>
2025-07-02 06:29:22 -0600

    fix: use dangerouslySetInnerHTML over react-html-parser

--- frontend/src/components/Blocks/Core/PostTitle/PostTitle.tsx
+++ frontend/src/components/Blocks/Core/PostTitle/PostTitle.tsx
@@ -1,6 +1,5 @@
 import { Maybe, CorePostTitleBlockAttributes } from '@/gql/graphql';
 import { getBlockClasses, getBlockStyleAttr, stripOuterTag } from '@/utils/getBlockComponents';
-import parse from 'html-react-parser';
 
 type PostTitleProps = {
   attributes: CorePostTitleBlockAttributes;
@@ -19,13 +18,16 @@ const PostTitle: React.FC<PostTitleProps> = ({
   const blockClasses = getBlockClasses(attributes, 'wp-block-post-title');
   const blockStyleAttr = getBlockStyleAttr(attributes);
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
-  const content = stripOuterTag(dynamicContent || saveContent || originalContent);
-  const html = parse(content ?? '');
+  const html = stripOuterTag(dynamicContent || saveContent || originalContent);
   
   return (
-    <Tag className={blockClasses} style={blockStyleAttr}>
-      {html || <span className="text-gray-500">No title available</span>}
-    </Tag>
+    <Tag
+      className={blockClasses}
+      style={blockStyleAttr}
+      dangerouslySetInnerHTML={{
+        __html: html || ''
+      }}
+    />
   );
 };
 
