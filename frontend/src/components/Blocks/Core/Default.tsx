e3a7094c9fa14f7a3d9959d96128b90cef4ca4b6
Matthew Ediger <edigermatthew@gmail.com>
2025-07-02 06:17:43 -0600

    fix: remove blank line

--- frontend/src/components/Blocks/Core/Default.tsx
+++ frontend/src/components/Blocks/Core/Default.tsx
@@ -21,15 +21,26 @@ export default function Default({
   const blockStyleAttr = getBlockStyleAttr(style);
   const Tag = tagName || 'div';
   const TagComponent = Tag as keyof JSX.IntrinsicElements;
+  const hasInnerBlocks = Array.isArray(innerBlocks) && innerBlocks.length > 0;
+  const html = dynamicContent || saveContent || originalContent;
 
-  return (
-    <TagComponent
-      {...(anchor && { id: anchor })}
-      className={blockClasses}
-      {...(style && { style: blockStyleAttr })}
-      {...(!innerBlocks || innerBlocks.length <= 0 ? { dangerouslySetInnerHTML: { __html: dynamicContent || saveContent || originalContent }} : {} )}
-    >
-      {innerBlocks && innerBlocks.length > 0 ? innerBlocks : null}
-    </TagComponent>
-  );
+  if (hasInnerBlocks) {
+    // If you want to wrap innerBlocks in the tag:
+    return (
+      <TagComponent
+        {...(anchor && { id: anchor })}
+        className={blockClasses}
+        {...(style && { style: blockStyleAttr })}
+      >
+        {innerBlocks}
+      </TagComponent>
+    );
+  }
+
+  if (html) {
+    // If html already includes the root tag, render as-is:
+    return <div dangerouslySetInnerHTML={{ __html: html }} />;
+  }
+
+  return null;
 }
