957b87e58525f31a8ac05ecb0871f539472fa200
Matthew Ediger <edigermatthew@gmail.com>
2025-07-02 06:11:09 -0600

    feat: initial commit

---
+++ frontend/src/components/Blocks/Core/PostTitle/PostTitle.tsx
@@ -0,0 +1,33 @@
+import { Maybe, CorePostTitleBlockAttributes } from '@/gql/graphql';
+import { getBlockClasses, getBlockStyleAttr, stripOuterTag } from '@/utils/getBlockComponents';
+import parse from 'html-react-parser';
+
+type PostTitleProps = {
+  attributes: CorePostTitleBlockAttributes;
+  dynamicContent?: Maybe<string> | undefined;
+  originalContent?: Maybe<string> | undefined;
+  saveContent?: Maybe<string> | undefined;
+};
+
+const PostTitle: React.FC<PostTitleProps> = ({
+  attributes,
+  dynamicContent,
+  originalContent,
+  saveContent
+}: PostTitleProps) => {
+  const { level, style } = attributes;
+  const blockClasses = getBlockClasses(attributes, 'wp-block-post-title');
+  const blockStyleAttr = getBlockStyleAttr(attributes);
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+  const content = stripOuterTag(dynamicContent || saveContent || originalContent);
+
+  return (
+    <Tag className={blockClasses} style={blockStyleAttr}>
+      {parse(content ?? '') || (
+        <span className="text-gray-500">No title available</span>
+      ) }
+    </Tag>
+  );
+};
+
+export default PostTitle;
