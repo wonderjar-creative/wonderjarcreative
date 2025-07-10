1c08675a3e1369b59d2e6b9a2cc597dcfc135372
Matthew Ediger <edigermatthew@gmail.com>
2025-07-09 22:24:00 -0600

    feat: use next/link

--- frontend/src/components/Blocks/Core/Button/Button.tsx
+++ frontend/src/components/Blocks/Core/Button/Button.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link';
 import { Maybe, CoreButtonBlockAttributes } from '@/gql/graphql';
-import { getBlockClasses, getBlockStyleAttr, stripOuterTags } from '@/utils/getBlockComponents';
+import { getBlockClasses, getBlockStyleAttr, stripOuterTag, getTransformedHTML } from '@/utils/getBlockComponents';
 
 type ButtonProps = {
   attributes: CoreButtonBlockAttributes;
+    anchor?: Maybe<string> | undefined;
+    style?: Maybe<Record<string, string>> | undefined;
+    url?: Maybe<string> | undefined;
+    target?: Maybe<string> | undefined;
+    rel?: Maybe<string> | undefined;
   dynamicContent?: Maybe<string> | undefined;
   originalContent?: Maybe<string> | undefined;
   saveContent?: Maybe<string> | undefined;
@@ -17,21 +23,23 @@ export default function Button({
   const { anchor, style, url, target, rel } = attributes;
   const blockClasses = getBlockClasses(attributes, 'wp-block-button');
   const blockStyleAttr = getBlockStyleAttr(style);
-  const html = stripOuterTags(dynamicContent || saveContent || originalContent);
-
+  const linkClasses = 'wp-block-button__link inline-block px-6 py-4 rounded-lg bg-deep-black font-semibold text-center text-soft-cream transition-colors hover:bg-charcoal-gray';
+  const content = dynamicContent || saveContent || originalContent || '';
+  const strippedDiv = stripOuterTag(content, 'div');
+  const strippedA = stripOuterTag(strippedDiv, 'a');
   return (
     <div
       {...(anchor && { id: anchor })}
       className={blockClasses}
       {...(style && { style: blockStyleAttr })}
     >
-      <a
-        href={url || '#'}
-        className="wp-block-button__link"
-        {...(target && { target })}
-        {...(rel && { rel })}
-        dangerouslySetInnerHTML={{ __html: html || '' }}
-      />
+      {url && (url.startsWith('/') || url.startsWith('http://localhost:3000')) ? (
+        <Link className={`${linkClasses} is-next-link _direct`} href={url} target={target} rel={rel}>
+          {getTransformedHTML(strippedA || '')}
+        </Link>
+      ) : (
+        getTransformedHTML(strippedDiv || '')
+      )}
     </div>
   );
 }
