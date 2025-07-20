26237fe6d97a621cb7b93dd0fbe817771f4d21f7
Matthew Ediger <edigermatthew@gmail.com>
2025-07-19 23:41:26 -0600

    fix: spelling of HTML to Html in fn name

--- frontend/src/components/Blocks/Core/Heading/Heading.tsx
+++ frontend/src/components/Blocks/Core/Heading/Heading.tsx
@@ -1,26 +1,21 @@
 import { Maybe, CoreHeadingBlockAttributes } from '@/gql/graphql';
-import { getBlockClasses, getBlockStyleAttr, stripOuterTag } from '@/utils/getBlockComponents';
-import parse from 'html-react-parser';
+import { getBlockClasses, getBlockStyleAttr } from '@/utils/blockStyles';
+import { stripOuterTag, getTransformedHtml } from '@/utils/htmlTransformations';
 
-type HeadingProps = {
+interface HeadingProps {
   attributes: CoreHeadingBlockAttributes;
   dynamicContent?: Maybe<string> | undefined;
   originalContent?: Maybe<string> | undefined;
   saveContent?: Maybe<string> | undefined;
 }
 
-export default function Heading({
-  attributes,
-  dynamicContent,
-  originalContent,
-  saveContent,
-}: HeadingProps) {
+const Heading: React.FC<HeadingProps> = ({ attributes, dynamicContent, originalContent, saveContent }) => {
   const { level, anchor, style, textAlign } = attributes;
   const blockClasses = getBlockClasses(attributes, 'wp-block-heading');
   const blockStyleAttr = getBlockStyleAttr(style);
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
   const content = stripOuterTag(dynamicContent || saveContent || originalContent || '', Tag as string);
-  const html = parse(content ?? '');
+  const html = getTransformedHtml(content || '');
 
   return (
     <Tag
@@ -31,4 +26,6 @@ export default function Heading({
       {html}
     </Tag>
   );
-}
+};
+
+export default Heading;
