bf486c55d4f877d182ba54711213805a02060275
Matthew Ediger <edigermatthew@gmail.com>
2025-07-23 22:51:18 -0600

    fix: import CorePostTitleBlock

--- frontend/src/components/Blocks/Core/PostTitle/PostTitle.tsx
+++ frontend/src/components/Blocks/Core/PostTitle/PostTitle.tsx
@@ -1,22 +1,16 @@
-import { Maybe, CorePostTitleBlockAttributes } from '@/gql/graphql';
-import { getBlockClasses, getBlockStyleAttr } from '@/utils/blockStyles';
+import { CorePostTitleBlock } from '@/utils/blockTypes';
+import { getBlockBaseClass, getBlockClasses, getBlockStyleAttr } from '@/utils/blockStyles';
 import { stripOuterTag } from '@/utils/htmlTransformations';
 import React from 'react';
 
-interface PostTitleProps {
-  attributes: CorePostTitleBlockAttributes;
-  dynamicContent?: Maybe<string> | undefined;
-  originalContent?: Maybe<string> | undefined;
-  saveContent?: Maybe<string> | undefined;
-};
-
-const PostTitle: React.FC<PostTitleProps> = ({ attributes, dynamicContent, originalContent, saveContent }) => {
-  const { level, style } = attributes;
-  const blockClasses = getBlockClasses(attributes, 'wp-block-post-title');
-  const blockStyleAttr = getBlockStyleAttr(attributes);
+const PostTitle: React.FC<CorePostTitleBlock> = ({ name, attributes, isDynamic, dynamicContent, saveContent }) => {
+  const { level, style } = attributes || {};
+  const blockClasses = getBlockClasses(attributes || {}, getBlockBaseClass(name));
+  const blockStyleAttr = getBlockStyleAttr(attributes || {});
   const Tag = `h${level}` || 'h2';
   const TagComponent = Tag as keyof JSX.IntrinsicElements;
-  const html = stripOuterTag(dynamicContent || saveContent || originalContent || '', Tag);
+  const content = dynamicContent || saveContent;
+  const html = stripOuterTag(content || '', Tag);
 
   return (
     <TagComponent
