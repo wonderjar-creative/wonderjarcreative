81bd680ba4a729968c6e5d2c7c357a1588f91169
Matthew Ediger <edigermatthew@gmail.com>
2025-07-23 22:58:13 -0600

    fix: import CoreGroupBlock

--- frontend/src/components/Blocks/Core/Group/Group.tsx
+++ frontend/src/components/Blocks/Core/Group/Group.tsx
@@ -1,14 +1,9 @@
-import { Maybe, CoreGroupBlockAttributes } from '@/gql/graphql';
+import { CoreGroupBlock } from '@/utils/blockTypes';
 import { getBlockClasses, getBlockStyleAttr } from '@/utils/blockStyles';
 
-interface GroupProps {
-  attributes: CoreGroupBlockAttributes;
-  innerBlocks?: React.ReactNode[];
-}
-
-const Group: React.FC<GroupProps> = ({ attributes, innerBlocks }) => {
-  const { anchor, style, tagName } = attributes;
-  const blockClasses = getBlockClasses(attributes, 'wp-block-group');
+const Group: React.FC<CoreGroupBlock> = ({ attributes, innerBlocks }) => {
+  const { anchor, style, tagName } = attributes || {};
+  const blockClasses = getBlockClasses(attributes || {}, 'wp-block-group');
   const blockStyleAttr = getBlockStyleAttr(style);
   const Tag = tagName || 'div';
   const TagComponent = Tag as keyof JSX.IntrinsicElements;
