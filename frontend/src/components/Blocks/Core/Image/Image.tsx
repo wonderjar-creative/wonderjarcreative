59a59864759881947df864d50d091d4ad98cee92
Matthew Ediger <edigermatthew@gmail.com>
2025-07-23 22:53:09 -0600

    fix: import CoreImageBlock

--- frontend/src/components/Blocks/Core/Image/Image.tsx
+++ frontend/src/components/Blocks/Core/Image/Image.tsx
@@ -1,36 +1,17 @@
 import Image from 'next/image';
 import { CoreImageBlockAttributes } from '@/gql/graphql';
-import { getBlockClasses, getBlockStyleAttr } from '@/utils/blockStyles';
+import { CoreImageBlock } from '@/utils/blockTypes';
+import { getBlockBaseClass, getBlockClasses, getBlockStyleAttr } from '@/utils/blockStyles';
 import { getMediaSize, getSizesAttribute } from '@/utils/blockMedia';
 import { stripOuterTag } from '@/utils/htmlTransformations';
 
-interface ImageProps {
-  attributes: CoreImageBlockAttributes;
-  name: string;
-  mediaItem?: {
-    node?: {
-      sourceUrl?: string;
-      altText?: string;
-      mediaDetails?: {
-        width?: number;
-        height?: number;
-        sizes?: Record<string, { 
-          sourceUrl: string;
-          width: number;
-          height: number;
-        }>;
-      }
-    }
-  };
-};
-
-const ImageComponent: React.FC<ImageProps> = ({ attributes, name, mediaItem }) => {
-  const { url, alt, anchor, aspectRatio, style, width, height, id, scale, sizeSlug } = attributes;
+const ImageComponent: React.FC<CoreImageBlock> = ({ name, attributes, mediaItem }) => {
+  const { url, align, alt, anchor, aspectRatio, style, width, height, scale, sizeSlug, ...imageAttributes } = attributes || {};
   const { sizes } = mediaItem?.node?.mediaDetails || {};
 
-  const blockClasses = getBlockClasses(attributes, 'wp-block-image');
+  const blockClasses = getBlockClasses(attributes || {}, `${getBlockBaseClass(name)} ${width || height ? 'is-resized' : '' }`);
   const blockStyleAttr = getBlockStyleAttr(style);
-  const imageClasses = `wp-block-image__img${id ? ` wp-image-${id}` : ''}`;
+  const imageClasses = getBlockClasses(imageAttributes, `wp-image ${align === 'center' ? 'mx-auto' : ''}`);
 
   const sizeObj = getMediaSize(sizeSlug, sizes);
   const imageSrc = sizeObj?.sourceUrl || url || mediaItem?.node?.sourceUrl || '';
