7b84c920043b64ee72f7a4a4f85932f1d4d6cca2
Matthew Ediger <edigermatthew@gmail.com>
2025-10-15 00:05:18 -0600

    chore: update template from editor

--- web/wordpress/wp-content/themes/wonderjarcreative-backend/patterns/header.php
+++ web/wordpress/wp-content/themes/wonderjarcreative-backend/patterns/header.php
@@ -16,6 +16,6 @@
 <div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide"><!-- wp:site-logo {"width":160,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} /-->
 
-<!-- wp:navigation {"ref":372} /--></div>
+<!-- wp:navigation {"ref":372,"layout":{"type":"flex","flexWrap":"wrap"}} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
