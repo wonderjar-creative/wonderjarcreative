7e56aa4cca8fe6bb95f5c0e9b59f0b4dddc6a26b
Matthew Ediger <edigermatthew@gmail.com>
2025-10-27 22:10:09 -0600

    feat: initial commit

---
+++ web/wordpress/wp-content/themes/wonderjarcreative-backend/patterns/header-absolute.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Header Absolute
+ * Slug: wonderjarcreative-backend/header-absolute
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description: Absolute site header with site title and navigation.
+ *
+ * @package WonderjarCreativeBackend
+ * @since 1.0.0
+ * @author wjmatt <matt@wonderjarcreative.com>
+ * @link https://wonderjarcreative.com
+ */
+?>
+<!-- wp:group {"className":"absolute left-0 right-0 z-10","style":{"position":{"type":""}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group absolute left-0 right-0 z-10"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-logo {"width":160,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} /-->
+
+<!-- wp:navigation {"ref":372,"textColor":"deep-black","style":{"typography":{"fontStyle":"normal","fontWeight":"500"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ web/wordpress/wp-content/themes/wonderjarcreative-backend/patterns/header-sticky.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Header Sticky
+ * Slug: wonderjarcreative-backend/header-sticky
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description: Sticky site header with site title and navigation.
+ *
+ * @package WonderjarCreativeBackend
+ * @since 1.0.0
+ * @author wjmatt <matt@wonderjarcreative.com>
+ * @link https://wonderjarcreative.com
+ */
+?>
+<!-- wp:group {"style":{"position":{"type":""}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-logo {"width":160,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} /-->
+
+<!-- wp:navigation {"ref":372,"textColor":"deep-black"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
