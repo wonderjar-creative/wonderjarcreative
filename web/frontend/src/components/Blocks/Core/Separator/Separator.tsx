a6556fab3dc4c8ee714784a9cc1637c9a8df7a27
Matthew Ediger <edigermatthew@gmail.com>
2025-11-20 23:47:52 -0700

    fix: move interface to coreBlockTypes.ts

--- web/frontend/src/components/Blocks/Core/Separator/Separator.tsx
+++ web/frontend/src/components/Blocks/Core/Separator/Separator.tsx
@@ -1,14 +1,9 @@
 import { CoreSeparatorBlockAttributes } from '@/gql/graphql';
 import { getBlockBaseClass, getBlockClasses, getBlockStyleAttr } from '@/utils/blockStyles';
+import { CoreSeparatorBlock } from '@/types/coreBlockTypes';
 
-interface SeparatorProps {
-  name: string;
-  attributes?: CoreSeparatorBlockAttributes;
-}
-
-const Separator: React.FC<SeparatorProps> = ({ name, attributes }) => {
+const Separator: React.FC<CoreSeparatorBlock> = ({ name, attributes }) => {
   const { anchor, style, ...separatorAttributes } = attributes || {};
-
   const blockClasses = getBlockClasses(separatorAttributes, getBlockBaseClass(name));
   const blockStyleAttr = getBlockStyleAttr(style);
 
