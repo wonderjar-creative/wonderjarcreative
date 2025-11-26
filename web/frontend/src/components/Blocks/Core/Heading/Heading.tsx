fce6f40d5d66356d4da5e5b6a3bf5e0cdf83e80d
Matthew Ediger <edigermatthew@gmail.com>
2025-11-26 00:25:44 -0700

    feat: add AnimatedBorder effect

--- web/frontend/src/components/Blocks/Core/Heading/Heading.tsx
+++ web/frontend/src/components/Blocks/Core/Heading/Heading.tsx
@@ -1,14 +1,30 @@
 import { CoreHeadingBlock } from '@/types/coreBlockTypes';
 import { getBlockBaseClass, getBlockClasses, getBlockStyleAttr } from '@/utils/blockStyles';
 import { stripOuterTag, getTransformedHtml } from '@/utils/htmlTransformations';
+import { AnimatedBorder } from '@/components/Effects';
 
 const Heading: React.FC<CoreHeadingBlock> = ({ name, attributes, saveContent }) => {
-  const { anchor, level, style } = attributes || {};
+  const { anchor, className, level, style } = attributes || {};
   const blockClasses = getBlockClasses(attributes || {}, getBlockBaseClass(name));
   const blockStyleAttr = getBlockStyleAttr(style);
   const Tag = `h${level}` || 'h2';
   const content = stripOuterTag(saveContent || '', Tag);
   const html = getTransformedHtml(content || '') || '';
+
+  const animatedBorder = className?.includes('__animated-border');
+  if (animatedBorder) {
+    return (
+      <AnimatedBorder
+        {...(anchor && { id: anchor })}
+        className={blockClasses}
+        {...(style && { style: blockStyleAttr })}
+        borderDirection="right"
+        borderColor="bg-deep-black"
+      >
+        {html}
+      </AnimatedBorder>
+    );
+  }
 
   return (
     <Tag
