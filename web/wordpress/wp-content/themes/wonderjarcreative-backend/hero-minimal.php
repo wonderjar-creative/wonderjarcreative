7a1e286958ec70201beab930d81007f13131d63d
Matthew Ediger <edigermatthew@gmail.com>
2025-11-28 20:45:53 -0700

    feat: initial commit

---
+++ web/wordpress/wp-content/themes/wonderjarcreative-backend/hero-minimal.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Hero Minimal
+ * Slug: wonderjarcreative-backend/hero-minimal
+ * Categories: banner, hero
+ * Description: A minimal hero section with a centered title and description.
+ *
+ * @package WonderjarCreativeBackend
+ * @since 1.4.0
+ * @author wjmatt <matt@wonderjarcreative.com>
+ * @link https://wonderjarcreative.com
+ */
+
+?>
+<!-- wp:cover {"overlayColor":"teal","isUserOverlayColor":true,"minHeight":70,"minHeightUnit":"vh","isDark":false,"metadata":{"categories":["banner","hero"],"patternName":"wonderjarcreative-backend/hero-about","name":"Hero About"},"align":"full","className":"dot-pattern","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}}} -->
+<div class="wp-block-cover alignfull is-light dot-pattern" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);min-height:70vh"><span aria-hidden="true" class="wp-block-cover__background has-teal-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide","className":"text-3xl md:text-5xl lg:text-7xl","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
+<h1 class="wp-block-heading alignwide has-text-align-center text-3xl md:text-5xl lg:text-7xl" style="margin-bottom:var(--wp--preset--spacing--40)">Hero Title</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","className":"text-lg md:text-xl"} -->
+<p class="has-text-align-center text-lg md:text-xl">A few words describing the hero section.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
